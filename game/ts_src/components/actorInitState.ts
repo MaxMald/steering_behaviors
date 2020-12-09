4b96261c22636289873fbd8604f763919ed7da1d
Jorge zamudio <idv15c.jzamudio@uartesdigitales.edu.mx>
2020-12-09 03:38:25 -0700

    Create ActorInitState class

---
+++ game/ts_src/components/actorInitState.ts
@@ -0,0 +1,24 @@
+/**
+ * Universidad de Artes Digitales, Guadalajara - 2020
+ *
+ * @summary 
+ *
+ * @file actorInitState.ts
+ * @author Jorge Alexandro Zamudio Arredondo <alexzamudio_11@hotmail.com>
+ * @since December-08-2020
+ */
+
+import { V2 } from "../commons/stTypes";
+
+export class ActorInitState {
+
+  /****************************************************/
+  /* Public                                           */
+  /****************************************************/
+  
+  public _m_initMass : number;
+
+  public _m_initMaxSpeed : number;
+
+  public _m_initPosition : V2;
+}
