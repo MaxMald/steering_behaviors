7f594d4924c83fecf5dc961469e98cd535a57f2a
Jorge zamudio <idv15c.jzamudio@uartesdigitales.edu.mx>
2020-12-14 18:44:06 -0700

    Add force init state class to save the init values of forces

---
+++ game/ts_src/steeringBehavior/forceInitState.ts
@@ -0,0 +1,20 @@
+/**
+ * Universidad de Artes Digitales, Guadalajara - 2020
+ *
+ * @summary 
+ *
+ * @file forceInitState.ts
+ * @author Jorge Alexandro Zamudio Arredondo <alexzamudio_11@hotmail.com>
+ * @since December-14-2020
+ */
+
+ export class forceInitState 
+ {
+
+   /****************************************************/
+   /* Public                                           */
+   /****************************************************/
+   
+   public m_initMaxMagnitude : number;
+   
+ }
