77e5e7c36e3dc0bb3c76d02d3bc1c8da89d5e0f0
Max Maldonado <41883722+MaxMald@users.noreply.github.com>
2020-12-21 18:46:19 -0600

    replace video background for images, for better compatibility

--- game/assets/images/game_menu/menu_art.js
+++ game/assets/images/game_menu/menu_art.js
@@ -1,7 +1,7 @@
 {
 "frames": {
 	"active_music_button.png": {
-		"frame": {"x":359, "y":1168, "w":64, "h":64},
+		"frame": {"x":425, "y":1356, "w":64, "h":64},
 		"spriteSourceSize": {"x":0,"y":0,"w":64,"h":64},
 		"sourceSize": {"w":64,"h":64}
 	},
@@ -11,54 +11,59 @@
 		"sourceSize": {"w":1025,"h":769}
 	},
 	"design_credits.png": {
-		"frame": {"x":0, "y":1254, "w":356, "h":84},
+		"frame": {"x":552, "y":771, "w":356, "h":84},
 		"spriteSourceSize": {"x":0,"y":0,"w":356,"h":84},
 		"sourceSize": {"w":356,"h":84}
 	},
 	"disable_music_button.png": {
-		"frame": {"x":425, "y":1168, "w":63, "h":63},
+		"frame": {"x":1092, "y":692, "w":63, "h":63},
 		"spriteSourceSize": {"x":0,"y":0,"w":63,"h":63},
 		"sourceSize": {"w":63,"h":63}
 	},
 	"full_screen_hover.png": {
-		"frame": {"x":358, "y":1254, "w":64, "h":63},
+		"frame": {"x":359, "y":1356, "w":64, "h":63},
 		"spriteSourceSize": {"x":0,"y":0,"w":64,"h":63},
 		"sourceSize": {"w":64,"h":63}
 	},
 	"full_screen_normal.png": {
-		"frame": {"x":424, "y":1234, "w":63, "h":64},
+		"frame": {"x":910, "y":771, "w":63, "h":64},
 		"spriteSourceSize": {"x":0,"y":0,"w":63,"h":64},
 		"sourceSize": {"w":63,"h":64}
 	},
 	"game_logo.png": {
-		"frame": {"x":495, "y":1071, "w":429, "h":229},
+		"frame": {"x":502, "y":1168, "w":429, "h":229},
 		"spriteSourceSize": {"x":0,"y":0,"w":429,"h":229},
 		"sourceSize": {"w":429,"h":229}
 	},
 	"hover_music_button.png": {
-		"frame": {"x":496, "y":1002, "w":63, "h":63},
+		"frame": {"x":1027, "y":692, "w":63, "h":63},
 		"spriteSourceSize": {"x":0,"y":0,"w":63,"h":63},
 		"sourceSize": {"w":63,"h":63}
 	},
 	"humming_credits.png": {
-		"frame": {"x":496, "y":877, "w":357, "h":123},
+		"frame": {"x":933, "y":1168, "w":357, "h":123},
 		"spriteSourceSize": {"x":0,"y":0,"w":357,"h":123},
 		"sourceSize": {"w":357,"h":123}
 	},
 	"main_btn_bg.png": {
-		"frame": {"x":926, "y":1161, "w":236, "h":86},
+		"frame": {"x":1184, "y":1293, "w":236, "h":86},
 		"spriteSourceSize": {"x":0,"y":0,"w":236,"h":86},
 		"sourceSize": {"w":236,"h":86}
 	},
 	"menu_btn_bg.png": {
-		"frame": {"x":926, "y":1249, "w":236, "h":86},
+		"frame": {"x":1027, "y":604, "w":236, "h":86},
 		"spriteSourceSize": {"x":0,"y":0,"w":236,"h":86},
 		"sourceSize": {"w":236,"h":86}
 	},
 	"menu_btn_border.png": {
-		"frame": {"x":552, "y":771, "w":249, "h":98},
+		"frame": {"x":933, "y":1293, "w":249, "h":98},
 		"spriteSourceSize": {"x":0,"y":0,"w":249,"h":98},
 		"sourceSize": {"w":249,"h":98}
+	},
+	"menu_planet.png": {
+		"frame": {"x":0, "y":877, "w":500, "h":477},
+		"spriteSourceSize": {"x":0,"y":0,"w":500,"h":477},
+		"sourceSize": {"w":500,"h":477}
 	},
 	"particle.png": {
 		"frame": {"x":1027, "y":0, "w":300, "h":300},
@@ -66,37 +71,37 @@
 		"sourceSize": {"w":300,"h":300}
 	},
 	"particle2.png": {
-		"frame": {"x":1027, "y":302, "w":300, "h":300},
+		"frame": {"x":998, "y":771, "w":300, "h":300},
 		"spriteSourceSize": {"x":0,"y":0,"w":300,"h":300},
 		"sourceSize": {"w":300,"h":300}
 	},
 	"particle3.png": {
-		"frame": {"x":926, "y":771, "w":300, "h":300},
+		"frame": {"x":1027, "y":302, "w":300, "h":300},
 		"spriteSourceSize": {"x":0,"y":0,"w":300,"h":300},
 		"sourceSize": {"w":300,"h":300}
 	},
 	"progra_credits.png": {
-		"frame": {"x":0, "y":1168, "w":357, "h":84},
+		"frame": {"x":0, "y":1356, "w":357, "h":84},
 		"spriteSourceSize": {"x":0,"y":0,"w":357,"h":84},
 		"sourceSize": {"w":357,"h":84}
 	},
 	"ship_panel_0.png": {
-		"frame": {"x":0, "y":1071, "w":493, "h":95},
+		"frame": {"x":502, "y":1071, "w":493, "h":95},
 		"spriteSourceSize": {"x":0,"y":0,"w":493,"h":95},
 		"sourceSize": {"w":493,"h":95}
 	},
 	"ship_panel_1.png": {
-		"frame": {"x":0, "y":974, "w":494, "h":95},
+		"frame": {"x":502, "y":974, "w":494, "h":95},
 		"spriteSourceSize": {"x":0,"y":0,"w":494,"h":95},
 		"sourceSize": {"w":494,"h":95}
 	},
 	"ship_panel_2.png": {
-		"frame": {"x":0, "y":877, "w":494, "h":95},
+		"frame": {"x":502, "y":877, "w":494, "h":95},
 		"spriteSourceSize": {"x":0,"y":0,"w":494,"h":95},
 		"sourceSize": {"w":494,"h":95}
 	},
 	"steeria_btn_bg.png": {
-		"frame": {"x":926, "y":1073, "w":236, "h":86},
+		"frame": {"x":997, "y":1073, "w":236, "h":86},
 		"spriteSourceSize": {"x":0,"y":0,"w":236,"h":86},
 		"sourceSize": {"w":236,"h":86}
 	},
@@ -109,7 +114,7 @@
 },
 "meta": {
 	"image": "menu_art.png",
-	"size": {"w": 1329, "h": 1340},
+	"size": {"w": 1422, "h": 1442},
 	"scale": "1"
 }
 }
