93d37e10694569c90ad3cdb1a4babdc3accdb96c
Max Maldonado <41883722+MaxMald@users.noreply.github.com>
2020-12-23 11:52:01 -0600

    update: Seek Scene

--- game/ts_src/scenes/preload.ts
+++ game/ts_src/scenes/preload.ts
@@ -91,6 +91,13 @@ extends Phaser.Scene
       "images/tutorial_book/tutorial_book.js"
     );
 
+    loader.atlas
+    (
+      'seek_book',
+      "images/seek_book/seek_book.png",
+      "images/seek_book/seek_book.js"
+    );
+
     /****************************************************/
     /* Bitmap Fonts                                     */
     /****************************************************/
--- game/ts_src/scenes/sims/sceneArrival.ts
+++ game/ts_src/scenes/sims/sceneArrival.ts
@@ -217,4 +217,5 @@ extends Phaser.Scene
   /****************************************************/
   
   private _m_master : Master;
+  
 }
