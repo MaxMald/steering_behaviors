c296783ec715838d7bbedae8d1b8af847f4b6c2d
Max Maldonado <41883722+MaxMald@users.noreply.github.com>
2020-12-23 19:30:12 -0600

    update: Pursuit Scene

---
+++ game/assets/images/pursuit_book/pursuit_book.js
@@ -0,0 +1,35 @@
+{
+"frames": {
+	"pursuit_base.png": {
+		"frame": {"x":0, "y":302, "w":400, "h":300},
+		"spriteSourceSize": {"x":0,"y":0,"w":400,"h":300},
+		"sourceSize": {"w":400,"h":300}
+	},
+	"pursuit_pre_pos.png": {
+		"frame": {"x":0, "y":0, "w":400, "h":300},
+		"spriteSourceSize": {"x":0,"y":0,"w":400,"h":300},
+		"sourceSize": {"w":400,"h":300}
+	},
+	"pursuit_seek.png": {
+		"frame": {"x":0, "y":604, "w":400, "h":300},
+		"spriteSourceSize": {"x":0,"y":0,"w":400,"h":300},
+		"sourceSize": {"w":400,"h":300}
+	},
+	"pursuit_time.png": {
+		"frame": {"x":402, "y":0, "w":400, "h":300},
+		"spriteSourceSize": {"x":0,"y":0,"w":400,"h":300},
+		"sourceSize": {"w":400,"h":300}
+	},
+	"pursuit_velocity.png": {
+		"frame": {"x":402, "y":302, "w":400, "h":300},
+		"spriteSourceSize": {"x":0,"y":0,"w":400,"h":300},
+		"sourceSize": {"w":400,"h":300}
+	}
+
+},
+"meta": {
+	"image": "pursuit_book.png",
+	"size": {"w": 804, "h": 906},
+	"scale": "1"
+}
+}
