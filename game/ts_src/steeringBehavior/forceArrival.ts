918725b15c87d78e241e9919a3a67e51e2cdfee0
Jorge zamudio <idv15c.jzamudio@uartesdigitales.edu.mx>
2020-12-30 20:17:44 -0700

    Assign the init state values of Arrival Force on init

--- game/ts_src/steeringBehavior/forceArrival.ts
+++ game/ts_src/steeringBehavior/forceArrival.ts
@@ -79,6 +79,8 @@ implements IForce
     );
 
     this._m_arrivalInitState = new ArrivalInitState();
+    this._m_arrivalInitState.m_initMaxMagnitude = _force;
+    this._m_arrivalInitState.m_initArrivalRadius = _arrivalRadius;
 
     // Get debug manager
 
