1cd4e9b1d3a3f60aad5949900c4f1ee1be21c104
Jorge zamudio <idv15c.jzamudio@uartesdigitales.edu.mx>
2021-01-07 14:37:35 -0700

    Fix visual issue on image and recreate atlas image and json file

--- game/assets/images/avoidance_book/avoidance_book.js
+++ game/assets/images/avoidance_book/avoidance_book.js
@@ -1,22 +1,22 @@
 {
 "frames": {
 	"avoidance_base.png": {
-		"frame": {"x":0, "y":376, "w":500, "h":375},
+		"frame": {"x":0, "y":378, "w":500, "h":375},
 		"spriteSourceSize": {"x":0,"y":0,"w":500,"h":375},
 		"sourceSize": {"w":500,"h":375}
 	},
 	"avoidance_circle.png": {
-		"frame": {"x":501, "y":0, "w":500, "h":375},
+		"frame": {"x":501, "y":378, "w":500, "h":375},
 		"spriteSourceSize": {"x":0,"y":0,"w":500,"h":375},
 		"sourceSize": {"w":500,"h":375}
 	},
 	"avoidance_multiple_obstacle.png": {
-		"frame": {"x":501, "y":376, "w":500, "h":375},
-		"spriteSourceSize": {"x":0,"y":0,"w":500,"h":375},
-		"sourceSize": {"w":500,"h":375}
+		"frame": {"x":0, "y":0, "w":503, "h":377},
+		"spriteSourceSize": {"x":0,"y":0,"w":503,"h":377},
+		"sourceSize": {"w":503,"h":377}
 	},
 	"avoidance_obstacle.png": {
-		"frame": {"x":0, "y":0, "w":500, "h":375},
+		"frame": {"x":504, "y":0, "w":500, "h":375},
 		"spriteSourceSize": {"x":0,"y":0,"w":500,"h":375},
 		"sourceSize": {"w":500,"h":375}
 	}
@@ -24,7 +24,7 @@
 },
 "meta": {
 	"image": "avoidance_book.png",
-	"size": {"w": 1002, "h": 752},
+	"size": {"w": 1005, "h": 754},
 	"scale": "1"
 }
 }
