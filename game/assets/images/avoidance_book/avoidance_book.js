edacfee3b0c67480a6382b8ae8dcbf84b5dcca1d
Jorge zamudio <idv15c.jzamudio@uartesdigitales.edu.mx>
2021-01-07 14:32:08 -0700

    Fix name error on image and json file

--- game/assets/images/avoidance_book/avoidance_book.js
+++ game/assets/images/avoidance_book/avoidance_book.js
@@ -10,7 +10,7 @@
 		"spriteSourceSize": {"x":0,"y":0,"w":500,"h":375},
 		"sourceSize": {"w":500,"h":375}
 	},
-	"avoidance_multiple_obstalce.png": {
+	"avoidance_multiple_obstacle.png": {
 		"frame": {"x":501, "y":376, "w":500, "h":375},
 		"spriteSourceSize": {"x":0,"y":0,"w":500,"h":375},
 		"sourceSize": {"w":500,"h":375}
