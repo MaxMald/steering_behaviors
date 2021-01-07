aa7ee0fd091586cf2648de3bc258c18a0af371e5
Jorge zamudio <idv15c.jzamudio@uartesdigitales.edu.mx>
2021-01-07 02:25:59 -0700

    Fix visual issue on image and recreate atlas image and json file

--- game/assets/images/wander_book/wander_book.js
+++ game/assets/images/wander_book/wander_book.js
@@ -1,32 +1,32 @@
 {
 "frames": {
 	"wander_base.png": {
-		"frame": {"x":0, "y":376, "w":500, "h":375},
-		"spriteSourceSize": {"x":0,"y":0,"w":500,"h":375},
-		"sourceSize": {"w":500,"h":375}
-	},
-	"wander_displacement_angle.png": {
 		"frame": {"x":0, "y":0, "w":500, "h":375},
 		"spriteSourceSize": {"x":0,"y":0,"w":500,"h":375},
 		"sourceSize": {"w":500,"h":375}
 	},
-	"wander_displacement_circle.png": {
+	"wander_displacement_angle.png": {
 		"frame": {"x":0, "y":752, "w":500, "h":375},
 		"spriteSourceSize": {"x":0,"y":0,"w":500,"h":375},
 		"sourceSize": {"w":500,"h":375}
 	},
-	"wander_displacement_random.png": {
+	"wander_displacement_circle.png": {
 		"frame": {"x":501, "y":376, "w":500, "h":375},
 		"spriteSourceSize": {"x":0,"y":0,"w":500,"h":375},
 		"sourceSize": {"w":500,"h":375}
 	},
-	"wander_displacement_vector.png": {
+	"wander_displacement_random.png": {
 		"frame": {"x":501, "y":0, "w":500, "h":375},
 		"spriteSourceSize": {"x":0,"y":0,"w":500,"h":375},
 		"sourceSize": {"w":500,"h":375}
 	},
-	"wander_force.png": {
+	"wander_displacement_vector.png": {
 		"frame": {"x":501, "y":752, "w":500, "h":375},
+		"spriteSourceSize": {"x":0,"y":0,"w":500,"h":375},
+		"sourceSize": {"w":500,"h":375}
+	},
+	"wander_force.png": {
+		"frame": {"x":0, "y":376, "w":500, "h":375},
 		"spriteSourceSize": {"x":0,"y":0,"w":500,"h":375},
 		"sourceSize": {"w":500,"h":375}
 	}
